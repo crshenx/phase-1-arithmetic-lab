36423dd10617bf68218fbb4c78ec1b3e8cf35182
Chris Hendricks <crshenx@gmail.com>
2022-06-06 14:38:19 -0700

    commit tree adf344c0183dc724fa204d991f385049661cae9d

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+let num1 = 31;
+let num2 = 2;
+// let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10 + 1);
+let num3 = 12;
+let num4 = 3;
+// let mod = num3 / num4;
+let max = Math.max(20);
 
-// Write your code here
+// let multiply = () => num1 * num2;
+
+function multi(num1, num2) {
+  return num1 * num2;
+}
+
+let multiply = multi(num1, num2);
+
+const fart = (num3, num4) => num3 / num4;
+let mod = fart(num3, num4);
+// console.log(multiply(31, 2));
+// console.log(random);
